d790d09d4a65c4407b6a48b6398c0761573d2106
luccy <valbarracin2003@gmail.com>
2024-10-29 17:57:34 -0500

    Agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { BrowserModule/*, provideClientHydration */} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterOutlet } from '@angular/router';
+import { CabeceraComponent } from './cabecera/cabecera.component';
+import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabeceraComponent,
+    PiePaginaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    //AppRoutingModule
+    RouterOutlet
   ],
   providers: [
-    provideClientHydration()
+    //provideClientHydration()
   ],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule, {
+//usando modulos
+platformBrowserDynamic().bootstrapModule(AppModule/*,{
   ngZoneEventCoalescing: true
-})
+}*/)
   .catch(err => console.error(err));
