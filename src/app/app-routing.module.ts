7bd54d6c43ad89336bd990894231156233275384
luccy <valbarracin2003@gmail.com>
2024-10-29 18:31:55 -0500

    Agregando parte dinamica con el router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
-const routes: Routes = [];
+const routes: Routes = [//todo lo que viene despues del 
+  //Se añaden las rutas al componente dinamico
+  {path:'', component:PaginaInicialComponent}//cuandola ruta este en blanco por default poner pag inicial
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule/*, provideClientHydration */} from '@angular/platform-browser';
 
-//import { AppRoutingModule } from './app-routing.module';
+//el import de app routing module para que aparezca la parte dinamica
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterOutlet } from '@angular/router';
 import { CabeceraComponent } from './cabecera/cabecera.component';
 import { PiePaginaComponent } from './pie-pagina/pie-pagina.component';
+import { PaginaInicialComponent } from './pagina-inicial/pagina-inicial.component';
 
 @NgModule({
   declarations: [
+    //Aquí se ponen todos los componentes 
     AppComponent,
     CabeceraComponent,
-    PiePaginaComponent
+    PiePaginaComponent,
+    PaginaInicialComponent
   ],
   imports: [
     BrowserModule,
-    //AppRoutingModule
+    AppRoutingModule,//para la parte dinamica
     RouterOutlet
   ],
   providers: [
